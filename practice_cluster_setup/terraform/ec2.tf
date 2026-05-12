70d230a4530c8df6ab3eac9534f978cd63cdf2cf
Chandan Singh <chandan21288@gmail.com>
2026-05-12 11:17:46 +0530

    code correction

--- practice_cluster_setup/terraform/ec2.tf
+++ practice_cluster_setup/terraform/ec2.tf
@@ -6,7 +6,7 @@ provider "aws" {
 resource "aws_security_group" "terraform_sg" {
   name = "terraform_sg"
 
-  ingress = {
+  ingress{
     from_port   =   22
     to_port     =   22
     protocol    =  "tcp"
@@ -14,23 +14,25 @@ resource "aws_security_group" "terraform_sg" {
 
   }
 
-  egress = {
+  egress{
     from_port   =   0
     to_port     =   0
     protocol    =   "-1"
-    cidr_block  =   ["0.0.0.0/0"]
+    cidr_blocks  =   ["0.0.0.0/0"]
   }
 
 }
 
 resource "aws_instance" "controller_server" {
-  ami = "ami-01b40e1bcccae197a"
-  instance_type = "t3.micro"
-  key_name = "terraform-key"
 
-  vpc_security_group_ids = [aws_security_group.terraform_sg]
+  ami           = "ami-01b40e1bcccae197a"
+  instance_type = "t3.micro"
+  key_name      = "terraform-key"
+
+  vpc_security_group_ids = [aws_security_group.terraform_sg.id]
 
   tags = {
-    Name    =   "devops-controller"
+    Name = "devops-controller"
   }
+
 }
