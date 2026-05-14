b64789a2ceff9c571886b613b2064cd07be03855
Chandan Singh <chandan21288@gmail.com>
2026-05-14 14:32:57 +0530

    added roles

--- practice_cluster_setup/terraform/ec2.tf
+++ practice_cluster_setup/terraform/ec2.tf
@@ -14,6 +14,14 @@ resource "aws_security_group" "terraform_sg" {
 
   }
 
+  ingress{
+    from_port   =   80
+    to_port     =   80
+    protocol    =  "tcp"
+    cidr_blocks =   ["0.0.0.0/0"]
+
+  }
+
   egress{
     from_port   =   0
     to_port     =   0
