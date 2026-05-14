9937ffc8cd31a226e160b8c734b42824d1d8b6e4
Chandan Singh <chandan21288@gmail.com>
2026-05-14 12:21:23 +0530

    added tfbackend

--- practice_cluster_setup/terraform/backend.tf
+++ practice_cluster_setup/terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   
   backend "s3" {
-    bucket = "tf-bucket-8375f78d "
+    bucket = "tf-bucket-8375f78d"
     key = "practice_cluster/terraform.tfstate"
     region = "ap-south-1"
     encrypt = true
